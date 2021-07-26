75a66c572ef45eb00d4ea7796ac3e49b49673f04
hollidavis <hollidavis19@gmail.com>
2021-07-25 18:13:15 -0600

    text shadow added

--- app/Services/ImagesService.js
+++ app/Services/ImagesService.js
@@ -4,9 +4,7 @@ import Image from "../Models/Image.js";
 class ImagesService {
   async getImages() {
     const res = await imagesApi.get()
-    console.log(res.data)
     ProxyState.images = new Image(res.data)
-    console.log(ProxyState.images)
   }
 }
 
